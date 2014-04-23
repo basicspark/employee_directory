0b64d7921968ac0b430cca9feaf1020f1b4276c3
Russ Martin <russ.martin@russ>
2014-04-23 12:31:15 -0500

    Fix pagination renderer problem

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,5 +1,3 @@
-# Set will_paginate to use the Bootstrap render
-# Also set the default window sizes
-WillPaginate::ActionView::LinkRenderer = BootstrapPagination::Rails
+# Set will_paginate default window sizes
 WillPaginate::ViewHelpers.pagination_options[:inner_window] = 1
 WillPaginate::ViewHelpers.pagination_options[:outer_window] = 1
