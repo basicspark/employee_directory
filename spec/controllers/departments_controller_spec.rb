286bd070d5560449caa304b17032f06bdc17c44b
Russ Martin <russ.martin@russ>
2014-04-27 12:27:55 -0500

    Add tests of ajax

---
+++ spec/controllers/departments_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe DepartmentsController do
+  before do
+    @department = create :department
+    @user = create :user
+  end
+
+  describe "deleting via ajax" do
+
+    context "when logged in" do
+      before { log_in_user @user, no_capybara: true }
+
+      it "reduces the department count by 1" do
+       expect do
+         xhr :delete, :destroy, id: @department.id
+       end.to change(Department, :count).by(-1)
+      end
+
+      it "returns success" do
+        xhr :delete, :destroy, id: @department.id
+        expect(response).to be_success
+      end
+
+      it "deletes the correct department" do
+        xhr :delete, :destroy, id: @department.id
+        expect { @department.reload }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+
+    context "when not logged in" do
+
+      it "does not reduce the department count" do
+        expect do
+          xhr :delete, :destroy, id: @department.id
+        end.not_to change(Department, :count)
+      end
+
+      it "does not return success" do
+        xhr :delete, :destroy, id: @department.id
+        expect(response).not_to be_success
+      end
+
+      it "does not delete the department" do
+        xhr :delete, :destroy, id: @department.id
+        expect { @department.reload }.not_to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
+end
--- spec/support/authentication_utilities.rb
+++ spec/support/authentication_utilities.rb
@@ -1,6 +1,14 @@
 def log_in_user(user, options={})
-  visit login_path
-  fill_in "email", with: options[:email] || user.email.upcase
-  fill_in "password", with: options[:password] || user.password
-  click_button 'Log in'
+  if options[:no_capybara]
+    # Sign in without Capybara
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+  else
+    # Sign in using Capybara
+    visit login_path
+    fill_in "email", with: options[:email] || user.email.upcase
+    fill_in "password", with: options[:password] || user.password
+    click_button 'Log in'
+  end
 end
